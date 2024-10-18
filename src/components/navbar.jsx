1bbf77e8767fc669886973e464287a0180f3fa95
Gowtham-0306 <141541254+Gowtham-0306@users.noreply.github.com>
2024-10-18 13:54:41 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -119,23 +119,23 @@ console.log(order);
 
 
 var options = {
-    "key": "rzp_test_D27jYnumNsPdCW", // Enter the Key ID generated from the Dashboard
-    amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "key": "rzp_test_D27jYnumNsPdCW", // 
+    amount, // Amount is in currency subunits
    currency,
     "name": "Acme Corp", //your business name
     "description": "Test Transaction",
     "image": "https://example.com/your_logo",
-    "order_id": order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    "order_id": order.id, //
     "handler": function (response){
         alert("Thanks for the purchase")
         alert(response.razorpay_payment_id);
         alert(response.razorpay_order_id);
         alert(response.razorpay_signature)
     },
-    "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+    "prefill": { // 
         "name": "Gaurav Kumar", //your customer's name
         "email": "gaurav.kumar@example.com", 
-        "contact": "9000090000"  //Provide the customer's phone number for better conversion rates 
+        "contact": "9000090000"  //Provide the customer's phone 
     },
     "notes": {
         "address": "Razorpay Corporate Office"
